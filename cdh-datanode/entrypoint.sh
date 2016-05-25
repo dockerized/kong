84f999a97ef45ddc2638e50dc9cc36ad6384b2ca
Tony.Shao <xiocode@gmail.com>
2016-05-25 18:54:19 +0800

    update

--- cdh-datanode/entrypoint.sh
+++ cdh-datanode/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.kong 50
-update-alternatives --set hadoop-conf /etc/hadoop/conf.kong
-
-service hadoop-hdfs-datanode start
-service hadoop-yarn-nodemanager start
-service hadoop-mapreduce-historyserver start
+sudo service hadoop-hdfs-datanode start
+sudo service hadoop-yarn-nodemanager start
--- cdh-namenode/entrypoint.sh
+++ cdh-namenode/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.kong 50
-update-alternatives --set hadoop-conf /etc/hadoop/conf.kong
-
-service hadoop-hdfs-datanode start
-service hadoop-yarn-nodemanager start
+service hadoop-hdfs-namenode start
 service hadoop-mapreduce-historyserver start
+service hadoop-yarn-resourcemanager start
+service hadoop-yarn-proxyserver start
+
 
